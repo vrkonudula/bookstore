a409b7bdf9c4f6cb1b97ac0ca29dd3d516205570
vrkonudula <vkonudula@vt.edu>
2021-02-04 13:26:51 -0500

    removed cors

--- src/main/java/com/venkat/bookstore/controller/OrderController.java
+++ src/main/java/com/venkat/bookstore/controller/OrderController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/api/orders")
-@CrossOrigin(origins = "https://forever-bookstore.netlify.app/")
 public class OrderController {
 
     @Autowired
